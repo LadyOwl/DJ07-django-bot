e703fb38584b27f9e9044cb8774e1eedae254089
Юлия (Джулия) <sixquestions666@gmail.com>
2026-02-02 20:14:54 +0300

    Upgrade

--- bot/models.py
+++ bot/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
+
+class TelegramUser(models.Model):
+    telegram_id = models.BigIntegerField(unique=True)
+    username = models.CharField(max_length=255)
+
+    def __str__(self):
+        return f"{self.username} ({self.telegram_id})"
 # Create your models here.
